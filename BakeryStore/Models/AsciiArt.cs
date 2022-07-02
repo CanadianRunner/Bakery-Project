df37e153cd6f2f39c2c0bea5c2044ea14dde512e
Sean Keane <seankeane93@gmail.com>
2022-07-01 17:01:15 -0700

    added ASCII art to header of program

--- BakeryStore/Models/AsciiArt.cs
+++ BakeryStore/Models/AsciiArt.cs
@@ -1,24 +1,17 @@
 using System;
-using BakeryStore.Models;
 
-namespace BakeryStore
+namespace BakeryStore.Models
 {
-  public class Program
-  {
-    public static void Main()
+  public class Ascii
     {
-      // Console.WriteLine(Ascii.title);
-      // Console.Read();
-      string title = @"
+      public string title = @"
  _       __     __                             __           ____  _                    _          ____        __                   __
 | |     / /__  / /________  ____ ___  ___     / /_____     / __ \(_)__  _____________ ( )_____   / __ )____ _/ /_____  _______  __/ /
 | | /| / / _ \/ / ___/ __ \/ __ `__ \/ _ \   / __/ __ \   / /_/ / / _ \/ ___/ ___/ _ \|// ___/  / __  / __ `/ //_/ _ \/ ___/ / / / / 
 | |/ |/ /  __/ / /__/ /_/ / / / / / /  __/  / /_/ /_/ /  / ____/ /  __/ /  / /  /  __/ (__  )  / /_/ / /_/ / ,< /  __/ /  / /_/ /_/  
 |__/|__/\___/_/\___/\____/_/ /_/ /_/\___/   \__/\____/  /_/   /_/\___/_/  /_/   \___/ /____/  /_____/\__,_/_/|_|\___/_/   \__, (_)   
                                                                                                                          /____/      
+
       ";
-      Console.WriteLine(title);
-      Console.WriteLine("We're excited for you try our delicious Breads and Pastries!");
     }
   }
-}
